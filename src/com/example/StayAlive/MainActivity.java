c636af3663239cb9814bc9bb3c22ca85d717888f
Sergey Eshin <EshinSergey@ya.ru>
2013-02-11 01:55:51 +0400

    created alpha-version of logic

--- src/com/example/StayAlive/MainActivity.java
+++ src/com/example/StayAlive/MainActivity.java
@@ -12,12 +12,21 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class MainActivity extends Activity implements View.OnClickListener
 {
     /** Called when the activity is first created. */
     final int MENU_ALPHA_ID = 1;
-    TextView tvResult;
+    final int MENU_REMAINDER_ID = 2;
+    TextView tvResult,tvRemainder;
     Button btnAdd;
+    Calendar calendar;
+    Calendar calendar2;
+    PersonalBankLogic bankLogic;
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -26,7 +35,18 @@ public class MainActivity extends Activity implements View.OnClickListener
         btnAdd=(Button)findViewById(R.id.btnAdd);
         btnAdd.setOnClickListener(this);
         tvResult=(TextView)findViewById(R.id.tvResult);
+        tvRemainder=(TextView)findViewById(R.id.tvRemainder);
         registerForContextMenu(tvResult);
+        Date aDate = new Date(System.currentTimeMillis());
+
+        calendar = GregorianCalendar.getInstance();
+        calendar.setTime(aDate);
+        calendar2 = GregorianCalendar.getInstance();
+        calendar2.setTime(new Date(System.currentTimeMillis()));
+        bankLogic=new PersonalBankLogic(3000);
+        int countOfDays=bankLogic.calculateCountOfDaysToLive(true,true,true,true,500);
+        tvResult.setText(String.valueOf(countOfDays)+ " days");
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -41,33 +61,95 @@ public class MainActivity extends Activity implements View.OnClickListener
             case R.id.tvResult:
                 // добавляем пункты
                 menu.add(0, MENU_ALPHA_ID, 0, "Refresh");
-
+                menu.add(0, MENU_REMAINDER_ID, 0, "Check Remainder");
                 break;
         }
         super.onCreateContextMenu(menu, v, menuInfo);
     }
     @Override
     public boolean onContextItemSelected(MenuItem item) {
-        Animation animation =null;
+        Animation  animation= AnimationUtils.loadAnimation(this, R.anim.myalpha);
         switch (item.getItemId()){
             case MENU_ALPHA_ID:
-                animation= AnimationUtils.loadAnimation(this, R.anim.myalpha);
+                int countOfDays=bankLogic.calculateCountOfDaysToLive(true,true,true,true,500);
+                tvResult.setText(String.valueOf(countOfDays)+ " days");
+                tvResult.startAnimation(animation);
+                break;
+
+            case MENU_REMAINDER_ID:
+                int remainder= bankLogic.getRemainder();
+                String messageAboutRemainder="Ваш остаток составляет "+ String.valueOf(remainder)+" руб.";
+                /*
+                tvRemainder.setVisibility(View.VISIBLE);
+                tvRemainder.setText(messageAboutRemainder);
+                tvRemainder.startAnimation(animation);
+                tvRemainder.setVisibility(View.INVISIBLE);   */
+                Toast.makeText(this,messageAboutRemainder,Toast.LENGTH_SHORT).show();
                 break;
 
         }
-        tvResult.setText("8 days");
-        tvResult.startAnimation(animation);
+
+
+
         return  super.onContextItemSelected(item);
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // TODO Auto-generated method stub
+        StringBuilder sb = new StringBuilder();
 
+        // Выведем в TextView информацию о нажатом пункте меню
+
+        sb.append("\r\n itemId: "  + String.valueOf(item.getItemId()));
+
+        sb.append("\r\n title: "   + item.getTitle());
+        Toast.makeText(this,sb.toString(),Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
+    }
+    // обновление меню
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // TODO Auto-generated method stub
+        // пункты меню с ID группы = 1 видны, если в CheckBox стоит галка
+        //  menu.setGroupVisible("@+id/group1",isFirstGroup);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+      private int cool(int i)
+      {
+          int i1=3,i2=4;
+
+          return i-=(i1+i2);
+      }
     @Override
     public void onClick(View view) {
         switch (view.getId())
         {
             case  (R.id.btnAdd):
-                Toast.makeText(this,"Worked!",Toast.LENGTH_SHORT).show();
+
+
+                calendar2.add(Calendar.DAY_OF_MONTH, 1);
+                calendar.setTime(new Date(System.currentTimeMillis()));
+                SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy hh:mm:ss");
+                String dayOfWeek= calendar2.getDisplayName(Calendar.DAY_OF_WEEK,Calendar.LONG,java.util.Locale.ENGLISH);
+
+                Toast.makeText(this,dayOfWeek+String.valueOf(cool(7)),Toast.LENGTH_SHORT).show();
+
+                Toast.makeText(this,formatter.format(calendar2.getTime()),Toast.LENGTH_SHORT).show();
+                Toast.makeText(this,formatter.format(calendar.getTime()),Toast.LENGTH_SHORT).show();
+               // long DaysLeft= getDays(calendar.getTime(),calendar2.getTime());
+
+              //  Toast.makeText(this,"Days left are "+String.valueOf(DaysLeft) ,Toast.LENGTH_SHORT).show();
                 break;
 
         }
     }
-}
+}          /*Calendar calendar = GregorianCalendar.getInstance();
+calendar.set(Calendar.MONTH, 10);
+calendar.set(Calendar.DAY_OF_MONTH, 15);
+calendar.set(Calendar.YEAR, 2005);
+formatter = new SimpleDateFormat("MMM dd, yyyy");
+System.out.println("Before: " + formatter.format(calendar.getTime()));
+
+calendar.add(Calendar.DAY_OF_MONTH, 20);
+System.out.println("After: " + formatter.format(calendar.getTime()));*/
---
+++ src/com/example/StayAlive/PersonalBankLogic.java
@@ -0,0 +1,116 @@
+package com.example.StayAlive;
+
+
+import java.util.*;
+
+public class PersonalBankLogic {
+
+    private  final int UNIVERSITY_FOOD =150;
+    private final  int BUS_VRN =11;
+    private final  int BUS_LIPETSK =12;
+    private final int BUS_106 =19;
+    private final int KEFIR =39;
+    private final int CLEANING_WOMAN =60;
+    private final int INTERNET =300;
+    private final  int SALVE =350;
+    final  int TRAIN_TO_HOME =71;
+    final int BUS_TO_HOME =179;
+    private final int BREAD =12;
+    private final int MINIMAL_BALANCE=150;
+    private final int PIZZA=100;
+    final  String[] vacationDays={"Saturday","Sunday","Thursday"};
+    final String[] trainingDays={"Saturday","Monday","Wednesday"};
+    private   int RideOnBusOnBothEnds=2* BUS_VRN;
+    private int eveningFood= BREAD + KEFIR;
+    private  int TransportToHomeByTrain=RideOnBusOnBothEnds+ TRAIN_TO_HOME + BUS_106;
+    private  int TransportToHomeByBus= BUS_TO_HOME + BUS_LIPETSK *2+ BUS_VRN;
+    private   int universityDay =(UNIVERSITY_FOOD +RideOnBusOnBothEnds);
+    private int studyDay=universityDay+eveningFood;
+    private  int homeDay=2* KEFIR + BREAD +50;
+    int currentBalance;
+    private int reminder;
+
+
+    Calendar calendar;
+
+    public  PersonalBankLogic(int incomingMoney){
+        currentBalance=incomingMoney;
+        calendar=getCalendar();
+    }
+    public int calculateCountOfDaysToLive(boolean isPizzaAfterTennis,boolean isGoHomeByTrain,boolean isInternetExist,boolean  isSalveExist,int additionalSpend)
+    {
+        //init
+        String currentDayOfWeek;
+        int expenseToday;
+        int balance=currentBalance;
+        Calendar currentCalendar=getCalendar();
+        //int additional
+        additionalSpend+=isInternetExist?0: INTERNET ;
+        additionalSpend+=isSalveExist?0: SALVE;
+
+        int costRideToHome=isGoHomeByTrain? TransportToHomeByTrain:TransportToHomeByBus;
+
+        balance-=(costRideToHome+additionalSpend+CLEANING_WOMAN);
+
+        do
+        {
+             currentDayOfWeek = dayWeek(currentCalendar);
+             expenseToday=isHomeDay(currentDayOfWeek)?homeDay:studyDay;
+             expenseToday+=(isTrainingDay(currentDayOfWeek)&&isPizzaAfterTennis)?PIZZA:0;
+             balance-=expenseToday;
+             currentCalendar.add(Calendar.DAY_OF_MONTH, 1);
+
+        }while(balance>MINIMAL_BALANCE);
+
+        calendar.setTime(new Date(System.currentTimeMillis()));
+        int countDaysToBeProvided=calculateCountDaysBetweenDates(calendar.getTime(),currentCalendar.getTime());
+        reminder=balance;
+        return  countDaysToBeProvided;
+    }
+
+
+    public static String dayWeek(Calendar calendar)
+    {
+        return calendar.getDisplayName(Calendar.DAY_OF_WEEK,Calendar.LONG,Locale.ENGLISH);
+    }
+    private boolean isHomeDay(String currentDay)
+    {
+         return (Arrays.asList(vacationDays).contains(currentDay))   ;
+
+    }
+    private Calendar getCalendar()
+    {
+        Date aDate = new Date(System.currentTimeMillis());
+        Calendar newCalendar = GregorianCalendar.getInstance();
+        newCalendar.setTime(aDate);
+        return newCalendar;
+    }
+    private boolean isTrainingDay(String currentDay)
+    {
+        return (Arrays.asList(trainingDays).contains(currentDay))   ;
+
+    }
+    private int calculateCountDaysBetweenDates(Date start, Date end)
+    {
+        calendar.setTime(start);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+
+        long l = calendar.getTimeInMillis();
+
+        calendar.setTime(end);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+
+        l = calendar.getTimeInMillis() - l;
+        return (int) (l / (24 * 60 * 60 * 1000));
+    }
+
+    public int getRemainder() {
+        return reminder;
+    }
+}
