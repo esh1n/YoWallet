e4d542767b2e3b8035d1d281c46d8b2717f86816
SunShelbi <qwerty92>
2013-02-13 17:40:01 +0400

    Managing personal settings is done

--- src/com/example/StayAlive/MainActivity.java
+++ src/com/example/StayAlive/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.StayAlive;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,43 +15,43 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.*;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-
-public class MainActivity extends Activity implements View.OnClickListener
-{
-    /** Called when the activity is first created. */
-    String[] persons={"Сергей","Арсен","Катя","Женя"};
+public class MainActivity extends Activity implements View.OnClickListener, SharedPreferences.OnSharedPreferenceChangeListener {
+    /**
+     * Called when the activity is first created.
+     */
+    public static final String APP_PREFERENCES = "mysettings"; // это будет именем файла настроек
+    SharedPreferences mSettings;
+    String[] persons = {"Сергей", "Арсен", "Катя", "Женя"};
+    final int REQUEST_CODE_OPERATION = 1;
+    final int REQUEST_CODE_SETTINGS = 2;
     final int MENU_ALPHA_ID = 1;
     final int MENU_REMAINDER_ID = 2;
-    final int MENU_BALANCE_ID=3;
-    TextView tvResult,tvRemainder;
+    final int MENU_BALANCE_ID = 3;
+    TextView tvResult, tvRemainder;
     Button btnAdd;
     PersonalBankLogic bankLogic;
     SharedPreferences settingsScreen;
+
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        btnAdd=(Button)findViewById(R.id.btnAdd);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
         btnAdd.setOnClickListener(this);
-        tvResult=(TextView)findViewById(R.id.tvResult);
-        tvRemainder=(TextView)findViewById(R.id.tvRemainder);
+        tvResult = (TextView) findViewById(R.id.tvResult);
+        tvRemainder = (TextView) findViewById(R.id.tvRemainder);
         registerForContextMenu(tvResult);
 
-        bankLogic=new PersonalBankLogic(3000);
-        int countOfDays=bankLogic.calculateCountOfDaysToLive(true,true,true,true,500);
-        tvResult.setText(String.valueOf(countOfDays)+ " дней");
+        bankLogic = new PersonalBankLogic(3000);
+        int countOfDays = bankLogic.calculateCountOfDaysToLive(true, true, false, false, 500);
+        tvResult.setText(String.valueOf(countOfDays) + " дней");
 
         // ???????
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, persons);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         Spinner spinner = (Spinner) findViewById(R.id.spnPerson);
         spinner.setAdapter(adapter);
-        // ?????????
+
         spinner.setPrompt("Persons");
         // ???????? ???????
         spinner.setSelection(0);
@@ -59,21 +61,29 @@ public class MainActivity extends Activity implements View.OnClickListener
             public void onItemSelected(AdapterView<?> parent, View view,
                                        int position, long id) {
                 // ?????????? ??????? ???????? ????????
-               // Toast.makeText(getBaseContext(), "Position = " + position, Toast.LENGTH_SHORT).show();
+                // Toast.makeText(getBaseContext(), "Position = " + position, Toast.LENGTH_SHORT).show();
             }
+
             @Override
             public void onNothingSelected(AdapterView<?> arg0) {
             }
         });
 
 
-
-        settingsScreen= PreferenceManager.getDefaultSharedPreferences(this);
+        mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+        settingsScreen = PreferenceManager.getDefaultSharedPreferences(this);
     }
+
     protected void onResume() {
-        String listValue = settingsScreen.getString("list", "не выбрано");
-        Toast.makeText(this,"Значение списка - " + listValue,Toast.LENGTH_SHORT).show();
+
         super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+
+        // Toast.makeText(this,"work",Toast.LENGTH_SHORT).show();
+        super.onStart();
     }
 
     @Override
@@ -81,6 +91,7 @@ public class MainActivity extends Activity implements View.OnClickListener
         getMenuInflater().inflate(R.menu.menu_default, menu);
         return true;
     }
+
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v,
                                     ContextMenu.ContextMenuInfo menuInfo) {
@@ -89,57 +100,59 @@ public class MainActivity extends Activity implements View.OnClickListener
             case R.id.tvResult:
                 // добавляем пункты
                 menu.add(0, MENU_ALPHA_ID, 0, "Refresh");
-                menu.add(0,MENU_BALANCE_ID,0,"Display Balance");
+                menu.add(0, MENU_BALANCE_ID, 0, "Display Balance");
                 menu.add(0, MENU_REMAINDER_ID, 0, "Check Remainder");
                 break;
         }
         super.onCreateContextMenu(menu, v, menuInfo);
     }
+
     @Override
     public boolean onContextItemSelected(MenuItem item) {
-        Animation  animation= AnimationUtils.loadAnimation(this, R.anim.myalpha);
-        switch (item.getItemId()){
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.myalpha);
+        switch (item.getItemId()) {
             case MENU_ALPHA_ID:
 
-                int countOfDays=bankLogic.calculateCountOfDaysToLive(true,true,true,true,500);
+                int countOfDays = bankLogic.calculateCountOfDaysToLive(true, true, false, false, 500);
                 displayDays(countOfDays);
                 break;
 
             case MENU_REMAINDER_ID:
-                int remainder= bankLogic.getRemainder();
-                String messageAboutRemainder="Ваш остаток составляет "+ String.valueOf(remainder)+" руб.";
+                int remainder = bankLogic.getRemainder();
+                String messageAboutRemainder = "Ваш остаток составляет " + String.valueOf(remainder) + " руб.";
                 /*
                 tvRemainder.setVisibility(View.VISIBLE);
                 tvRemainder.setText(messageAboutRemainder);
                 tvRemainder.startAnimation(animation);
                 tvRemainder.setVisibility(View.INVISIBLE);   */
-                Toast.makeText(this,messageAboutRemainder,Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, messageAboutRemainder, Toast.LENGTH_SHORT).show();
                 break;
             case MENU_BALANCE_ID:
-                int balance=bankLogic.getCurrentBalance();
-                String messageAboutBalance="Ваш текщий баланс составляет "+String.valueOf(balance) +"руб.";
-                Toast.makeText(this,messageAboutBalance,Toast.LENGTH_SHORT).show();
+                int balance = bankLogic.getCurrentBalance();
+                String messageAboutBalance = "Ваш текщий баланс составляет " + String.valueOf(balance) + "руб.";
+                Toast.makeText(this, messageAboutBalance, Toast.LENGTH_SHORT).show();
                 break;
 
         }
 
 
-
-        return  super.onContextItemSelected(item);
+        return super.onContextItemSelected(item);
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getTitle().equals("Настройки"))
-        {
+        if (item.getTitle().equals("Настройки")) {
            /* StringBuilder sb = new StringBuilder();
             sb.append("\r\n itemId: "  + String.valueOf(item.getItemId()));
             sb.append("\r\n title: "   + item.getTitle());
             Toast.makeText(this,sb.toString(),Toast.LENGTH_SHORT).show();*/
-            startActivityForResult(new Intent(this, PrefActivity.class),1);
+            Intent settingsIntent = new Intent(this, PrefActivity.class);
+            startActivityForResult(settingsIntent, REQUEST_CODE_SETTINGS);
 
         }
         return super.onOptionsItemSelected(item);
     }
+
     // обновление меню
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
@@ -152,34 +165,83 @@ public class MainActivity extends Activity implements View.OnClickListener
 
     @Override
     public void onClick(View view) {
-        switch (view.getId())
-        {
-            case  (R.id.btnAdd):
+        switch (view.getId()) {
+            case (R.id.btnAdd):
 
                 Intent intent = new Intent(this, OperationActivity.class);
-                startActivityForResult(intent, 1);
+                startActivityForResult(intent, REQUEST_CODE_OPERATION);
                 break;
 
         }
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (data == null) {return;}
-        int summa = data.getIntExtra("summa",-1);
-        String operation=data.getStringExtra("operation");
-        Toast.makeText(this,"Операция '"+ operation+ "' на сумму "+String.valueOf(summa)+" проведена успешно.",Toast.LENGTH_SHORT).show();
+        // запишем в лог значения requestCode и resultCode
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("myLogs", "requestCode = " + requestCode + ", resultCode = " + resultCode);
 
-        bankLogic.AddOperation(summa,operation);
-        int countOfDays=bankLogic.calculateCountOfDaysToLive(true,true,true,true,500);
-        displayDays(countOfDays);
+        switch (requestCode) {
+            case REQUEST_CODE_SETTINGS:
+                updateUserSettings();
+                break;
+            case REQUEST_CODE_OPERATION:
+                if (data == null) {
+                    return;
+                }
+                int summa = data.getIntExtra("summa", -1);
+                String operation = data.getStringExtra("operation");
+                Toast.makeText(this, "Операция '" + operation + "' на сумму " + String.valueOf(summa) + " проведена успешно.", Toast.LENGTH_SHORT).show();
+                bankLogic.AddOperation(summa, operation);
+                int countOfDays = bankLogic.calculateCountOfDaysToLive(true, true, false, false, 500);
+                displayDays(countOfDays);
+                break;
 
+
+        }
+        // если вернулось не ОК
+      /*  } else {
+            if(resultCode== REQUEST_CODE_SETTINGS)  {
+                showUserSettings();
+            }
+
+            Toast.makeText(this, "Wrong result", Toast.LENGTH_SHORT).show();  */
+    }
+
+
+    private void displayDays(int countOfDays) {
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.myalpha);
+        tvResult.setText(String.valueOf(countOfDays) + " days");
+        tvResult.startAnimation(animation);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
 
     }
-    private void displayDays(int countOfDays)
-    {
-        Animation  animation= AnimationUtils.loadAnimation(this, R.anim.myalpha);
-        tvResult.setText(String.valueOf(countOfDays)+ " days");
-        tvResult.startAnimation(animation);
+
+    private void updateUserSettings() {
+        SharedPreferences sharedPrefs = PreferenceManager
+                .getDefaultSharedPreferences(this);
+        Boolean isPizzaAfterTennis = sharedPrefs.getBoolean("pizza", false);
+        Boolean isNeedInternet = sharedPrefs.getBoolean("inet", false);
+        Boolean isNeedSalve = sharedPrefs.getBoolean("salve", false);
+        String additionalExpenses = sharedPrefs.getString("addExpense", "120");
+        int expense = Integer.parseInt(additionalExpenses);
+        String wayToHome = sharedPrefs.getString("way", "не выбрано");
+        boolean isWayByTrain = wayToHome.equals("На электричке");
+        int countOfDays = bankLogic.calculateCountOfDaysToLive(isPizzaAfterTennis, isWayByTrain, isNeedInternet, isNeedSalve, expense);
+        displayDays(countOfDays);
+       /* String messageVerify=wayToHome+
+                " Pizza ->"+String.valueOf(isPizzaAfterTennis)+
+                " Spend -> "+String.valueOf(expense)+
+                " isNeedSalve "+String.valueOf(isNeedSalve)+
+                "isByTrain->"+String.valueOf(isWayByTrain)+
+                "isNeedInternet->"+String.valueOf(isNeedInternet);
+
+        Toast.makeText(this,messageVerify,Toast.LENGTH_LONG).show();   */
+
+
     }
 }          /*Calendar calendar = GregorianCalendar.getInstance();
 calendar.set(Calendar.MONTH, 10);
--- src/com/example/StayAlive/PersonalBankLogic.java
+++ src/com/example/StayAlive/PersonalBankLogic.java
@@ -9,110 +9,110 @@ public class PersonalBankLogic {
         return currentBalance;
     }
 
-    public  enum BankOperation{Deposit,WithDraw,Reserve}
-    private  final int UNIVERSITY_FOOD =150;
-    private final  int BUS_VRN =11;
-    private final  int BUS_LIPETSK =12;
-    private final int BUS_106 =19;
-    private final int KEFIR =39;
-    private final int CLEANING_WOMAN =60;
-    private final int INTERNET =300;
-    private final  int SALVE =350;
-    final  int TRAIN_TO_HOME =71;
-    final int BUS_TO_HOME =179;
-    private final int BREAD =12;
-    private final int MINIMAL_BALANCE=150;
-    private final int MAXIMUM_ON_PIZZA=300;
-    private final int PIZZA=100;
-    final  String[] vacationDays={"Saturday","Sunday","Thursday"};
-    final String[] trainingDays={"Saturday","Monday","Wednesday"};
-    private   int RideOnBusOnBothEnds=2* BUS_VRN;
-    private int eveningFood= BREAD + KEFIR;
-    private  int TransportToHomeByTrain=RideOnBusOnBothEnds+ TRAIN_TO_HOME + BUS_106;
-    private  int TransportToHomeByBus= BUS_TO_HOME + BUS_LIPETSK *2+ BUS_VRN;
-    private   int universityDay =(UNIVERSITY_FOOD +RideOnBusOnBothEnds);
-    private int studyDay=universityDay+eveningFood;
-    private  int homeDay=2* KEFIR + BREAD +50;
+    public enum BankOperation {Deposit, WithDraw, Reserve}
+
+    private final int UNIVERSITY_FOOD = 150;
+    private final int BUS_VRN = 11;
+    private final int BUS_LIPETSK = 12;
+    private final int BUS_106 = 19;
+    private final int KEFIR = 39;
+    private final int CLEANING_WOMAN = 60;
+    private final int INTERNET = 300;
+    private final int SALVE = 350;
+    final int TRAIN_TO_HOME = 71;
+    final int BUS_TO_HOME = 179;
+    private final int BREAD = 12;
+    private final int MINIMAL_BALANCE = 150;
+    private final int MAXIMUM_ON_PIZZA = 300;
+    private final int PIZZA = 100;
+    final String[] vacationDays = {"Saturday", "Sunday", "Thursday"};
+    final String[] trainingDays = {"Saturday", "Monday", "Wednesday"};
+    private int RideOnBusOnBothEnds = 2 * BUS_VRN;
+    private int eveningFood = BREAD + KEFIR;
+    private int TransportToHomeByTrain = RideOnBusOnBothEnds + TRAIN_TO_HOME + BUS_106;
+    private int TransportToHomeByBus = BUS_TO_HOME + BUS_LIPETSK * 2 + BUS_VRN;
+    private int universityDay = (UNIVERSITY_FOOD + RideOnBusOnBothEnds);
+    private int studyDay = universityDay + eveningFood;
+    private int homeDay = 2 * KEFIR + BREAD + 50;
     int currentBalance;
     private int reminder;
-    private int reserve=0;
+    private int reserve = 0;
 
     Calendar calendar;
 
-    public  PersonalBankLogic(int incomingMoney){
-        currentBalance=incomingMoney;
-        calendar=getCalendar();
+    public PersonalBankLogic(int incomingMoney) {
+        currentBalance = incomingMoney;
+        calendar = getCalendar();
     }
-    public int calculateCountOfDaysToLive(boolean isPizzaAfterTennis,boolean isGoHomeByTrain,boolean isInternetExist,boolean  isSalveExist,int additionalSpend)
-    {
+
+    public int calculateCountOfDaysToLive(boolean isPizzaAfterTennis, boolean isGoHomeByTrain, boolean isNeedInternet, boolean isNeedSalve, int additionalSpend) {
         //init
         String currentDayOfWeek;
         int expenseToday;
-        int balance=currentBalance;
-        Calendar currentCalendar=getCalendar();
+        int balance = currentBalance;
+        Calendar currentCalendar = getCalendar();
         //int additional
-        additionalSpend+=isInternetExist?0: INTERNET ;
-        additionalSpend+=isSalveExist?0: SALVE;
-        additionalSpend+=isPizzaAfterTennis?MAXIMUM_ON_PIZZA:0;
-        int costRideToHome=isGoHomeByTrain? TransportToHomeByTrain:TransportToHomeByBus;
+        additionalSpend += isNeedInternet ? INTERNET : 0;
+        additionalSpend += isNeedSalve ? SALVE : 0;
+        additionalSpend += isPizzaAfterTennis ? MAXIMUM_ON_PIZZA : 0;
+        int costRideToHome = isGoHomeByTrain ? TransportToHomeByTrain : TransportToHomeByBus;
 
-        balance-=(costRideToHome+additionalSpend+CLEANING_WOMAN);
+        balance -= (costRideToHome + additionalSpend + CLEANING_WOMAN);
 
-        do
-        {
-             currentDayOfWeek = dayWeek(currentCalendar);
-             expenseToday=isHomeDay(currentDayOfWeek)?homeDay:studyDay;
-             // expenseToday+=(isTrainingDay(currentDayOfWeek)&&isPizzaAfterTennis)?PIZZA:0;
-             balance-=expenseToday;
-             currentCalendar.add(Calendar.DAY_OF_MONTH, 1);
+        do {
+            currentDayOfWeek = dayWeek(currentCalendar);
+            expenseToday = isHomeDay(currentDayOfWeek) ? homeDay : studyDay;
+            // expenseToday+=(isTrainingDay(currentDayOfWeek)&&isPizzaAfterTennis)?PIZZA:0;
+            balance -= expenseToday;
+            currentCalendar.add(Calendar.DAY_OF_MONTH, 1);
 
-        }while(balance>MINIMAL_BALANCE);
+        } while (balance > MINIMAL_BALANCE);
 
         calendar.setTime(new Date(System.currentTimeMillis()));
-        int countDaysToBeProvided=calculateCountDaysBetweenDates(calendar.getTime(),currentCalendar.getTime());
-        reminder=balance;
-        return  countDaysToBeProvided;
+        int countDaysToBeProvided = calculateCountDaysBetweenDates(calendar.getTime(), currentCalendar.getTime());
+        reminder = balance;
+        return countDaysToBeProvided;
     }
+
     public int getRemainder() {
         return reminder;
     }
-    public void AddOperation(int summa,String bankOperation)
-    {
+
+    public void AddOperation(int summa, String bankOperation) {
         if (bankOperation.equals("Вклад"))
-            currentBalance+=summa;
-        if (bankOperation.equals("Отложить на запас"))
-        {
-            currentBalance-=summa;
-            reserve+=summa;
+            currentBalance += summa;
+        if (bankOperation.equals("Отложить на запас")) {
+            currentBalance -= summa;
+            reserve += summa;
         }
         if (bankOperation.equals("Снять"))
-            currentBalance-=summa;
+            currentBalance -= summa;
 
 
     }
-    private String dayWeek(Calendar calendar)
-    {
-        return calendar.getDisplayName(Calendar.DAY_OF_WEEK,Calendar.LONG,Locale.ENGLISH);
+
+    private String dayWeek(Calendar calendar) {
+        return calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.ENGLISH);
     }
-    private boolean isHomeDay(String currentDay)
-    {
-         return (Arrays.asList(vacationDays).contains(currentDay))   ;
+
+    private boolean isHomeDay(String currentDay) {
+        return (Arrays.asList(vacationDays).contains(currentDay));
 
     }
-    private Calendar getCalendar()
-    {
+
+    private Calendar getCalendar() {
         Date aDate = new Date(System.currentTimeMillis());
         Calendar newCalendar = GregorianCalendar.getInstance();
         newCalendar.setTime(aDate);
         return newCalendar;
     }
-    private boolean isTrainingDay(String currentDay)
-    {
-        return (Arrays.asList(trainingDays).contains(currentDay))   ;
+
+    private boolean isTrainingDay(String currentDay) {
+        return (Arrays.asList(trainingDays).contains(currentDay));
 
     }
-    private int calculateCountDaysBetweenDates(Date start, Date end)
-    {
+
+    private int calculateCountDaysBetweenDates(Date start, Date end) {
         calendar.setTime(start);
         calendar.set(Calendar.HOUR_OF_DAY, 0);
         calendar.set(Calendar.MINUTE, 0);
